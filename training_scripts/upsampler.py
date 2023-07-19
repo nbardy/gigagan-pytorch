8ddc014016aa5e0e4bb7c368c5a8bd90f140645e
Nicholas Bardy <nicholasbardy@gmail.com>
2023-07-19 16:57:17 -0700

    name

--- training_scripts/upsampler.py
+++ training_scripts/upsampler.py
@@ -22,7 +22,7 @@ else:
     device = torch.device("cpu")
 
 dataset = ImageDataset(
-    folder = '/Users/nicholasbardy/Upscale_test',
+    folder = '/home/nicholasbardy/Upscale_test',
     image_size = 256
 )
 
